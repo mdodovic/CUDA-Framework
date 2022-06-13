8e286c74cf5396f97b06cef6b904187147a8e11c
mdodovic <matija.dodovic@gmail.com>
2022-06-13 10:37:33 +0200

    Show graphics script; Add results; Add run scripts

---
+++ gameoflife/plot_graphics.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+
+def main():
+
+    simplex_modified_mpi_50000 = [1.514, 1.585, 1.595]
+    simplex_modified_mpi_100000 = [1.936, 2.115, 2.146]
+    simplex_modified_mpi_1000000 = [1.834, 2.16, 2.151]
+
+    plt.plot(num_of_threads, simplex_modified_mpi_50000)
+    plt.plot(num_of_threads, simplex_modified_mpi_100000)
+    plt.plot(num_of_threads, simplex_modified_mpi_1000000)
+    plt.xlabel("Broj niti")
+    plt.ylabel("Ubrzanje")
+    plt.legend(["50000 iterations", "100000 iterations", "1000000 iterations"], loc="best")
+    plt.savefig("simplex_modified.png", dpi = 90)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ gameoflife/run_sequential
@@ -0,0 +1,5 @@
+./gameoflife_sequential.exe 30 30 1000
+./gameoflife_sequential.exe 500 500 10
+./gameoflife_sequential.exe 1000 1000 100
+./gameoflife_sequential.exe 1000 1000 1000
+
