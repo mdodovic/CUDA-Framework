f234f9f13056c88c8ca52f64c47062ac3805eaae
mdodovic <matija.dodovic@gmail.com>
2022-06-13 10:46:08 +0200

    gameoflife show graphics

--- gameoflife/plot_graphics.py
+++ gameoflife/plot_graphics.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    simplex_modified_mpi_50000 = [1.514, 1.585, 1.595]
-    simplex_modified_mpi_100000 = [1.936, 2.115, 2.146]
-    simplex_modified_mpi_1000000 = [1.834, 2.16, 2.151]
+    num_of_threads = [2, 4, 8]
 
-    plt.plot(num_of_threads, simplex_modified_mpi_50000)
-    plt.plot(num_of_threads, simplex_modified_mpi_100000)
-    plt.plot(num_of_threads, simplex_modified_mpi_1000000)
+    gameoflife_mpi_30_30_1000 = [1.514, 1.585, 1.595]
+    gameoflife_mpi_500_500_10 = [1.936, 2.115, 2.146]
+    gameoflife_mpi_1000_1000_100 = [1.834, 2.16, 2.151]
+    gameoflife_mpi_1000_1000_1000 = [1.834, 2.16, 2.151]
+
+    plt.plot(num_of_threads, gameoflife_mpi_30_30_1000)
+    plt.plot(num_of_threads, gameoflife_mpi_500_500_10)
+    plt.plot(num_of_threads, gameoflife_mpi_1000_1000_100)
+    plt.plot(num_of_threads, gameoflife_mpi_1000_1000_1000)
     plt.xlabel("Broj niti")
     plt.ylabel("Ubrzanje")
-    plt.legend(["50000 iterations", "100000 iterations", "1000000 iterations"], loc="best")
-    plt.savefig("simplex_modified.png", dpi = 90)
+    plt.legend(["30x30/1000", "500x500/10", "1000x1000/100", "1000x1000/1000"], loc="best")
+    plt.savefig("./game_of_life.png", dpi = 90)
     plt.show()
 
 
