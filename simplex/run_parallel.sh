9df82ee489f92f08845639ba7d411c469eadf38b
mdodovic <matija.dodovic@gmail.com>
2022-07-07 10:46:25 +0200

    Run scripts

---
+++ simplex/run_parallel.sh
@@ -0,0 +1,3 @@
+./simplex_parallel.exe 50000
+./simplex_parallel.exe 100000
+./simplex_parallel.exe 1000000
---
+++ simplex/run_sequential.sh
@@ -0,0 +1,3 @@
+./simplex_sequential.exe 50000
+./simplex_sequential.exe 100000
+./simplex_sequential.exe 1000000
